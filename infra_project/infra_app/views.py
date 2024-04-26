f7c6ca9e5d318732e5ec52f4461e39d1ef578907
NITRO <edmondkoko777@gmail.com>
2024-04-26 13:57:32 +0300

    add tests

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -1,9 +1,9 @@
 from django.http import HttpResponse
+from django.http.request import HttpRequest
 
-
-def index(request):
+def index(request: HttpRequest) -> HttpResponse:
     return HttpResponse('У меня получилось!')
 
 
-def second_page(request):
+def second_page(request: HttpRequest) -> HttpResponse:
     return HttpResponse('А это вторая страница')
